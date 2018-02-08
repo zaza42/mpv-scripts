e18b45d158973b0af973c866664c2477f8401e58
DC-1 (Zaza) <DC-1@tecra.flerp>
2018-02-08 09:42:32 +0100

    shorter code

--- ytdl-autosub.lua
+++ ytdl-autosub.lua
@@ -3,30 +3,25 @@
 --   ( copy me to ~/.config/mpv/scripts/ )
 --
 -- required binaries:
--- mpv          - http://mpv.io
 -- youtube-dl   - https://youtube-dl.org/
--- ffmpeg       - https://ffmpeg.org/
 --
 local key_binding = "ctrl+v"
 local utils = require 'mp.utils'
 local msg = require 'mp.msg'
+local autosub = "/dev/shm/mpv-autosub"
 
 function getautosub()
 	filepath = mp.get_property("path")
 	if (filepath:find("http") ~= 1) then return end
---	msg.warn("download autosub")
+	msg.warn("download autosub")
         mp.osd_message("downloading auto-generated subtitles", osd_time)
 
         cmd = { args = { 'sh', '-c',
-	    "youtube-dl --skip-download --write-auto-sub -o /dev/shm/$PPID.vtt '" .. filepath .. "'"} }
+	    "youtube-dl --skip-download --write-auto-sub -o " .. autosub .. " '" .. filepath .. "'"} }
         utils.subprocess(cmd)
-        cmd = { args = { 'sh', '-c',
-	    "ffmpeg -i /dev/shm/$PPID.en.vtt /dev/shm/titleper.auto.srt"} }
-        utils.subprocess(cmd)
-	mp.command("sub_add /dev/shm/titleper.auto.srt")
-        cmd = { args = { 'sh', '-c',
-	    "rm /dev/shm/titleper.auto.srt /dev/shm/$PPID.*"} }
-        utils.subprocess(cmd)
+	mp.command("sub_add " .. autosub .. ".en.vtt")
+	os.remove(autosub .. ".en.vtt")
+	os.remove(autosub .. ".jpg")
 end
 
 mp.add_key_binding(key_binding, "autosub", getautosub)
