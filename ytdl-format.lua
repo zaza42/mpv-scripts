b6f0d7f524dbd2051a99b4b336430b8d0ef51832
DC-1 (Zaza) <DC-1@tecra.flerp>
2018-02-13 04:56:24 +0100

    fix on playlisy

--- ytdl-format.lua
+++ ytdl-format.lua
@@ -6,11 +6,18 @@ mp.add_hook("on_load", 9, function ()
     local url = mp.get_property("path")
 --    if (url:find("http://www.youtu") == 1)  or (url:find("https://www.youtu") == 1)
 --        or (url:find("http://youtu.be") == 1)  or (url:find("https://youtu.be") == 1)
+    if (url:find("http") ~= 1) then return end
+    mp.set_property("options/ytdl-format", "")
+    mp.set_property("options/ytdl-raw-options", "")
     if ( string.find(url, 'youtu') )
     then
         msg.warn("youtube on_load matched")
         mp.set_property("options/ytdl-format", "bestvideo[height<=1080]+bestaudio")
 --        msg.warn(mp.get_property("stream-open-filename"))
+    elseif ( string.find(url, 'nicovideo') )
+    then
+        msg.warn("nicovideo on_load matched")
+        mp.set_property("options/ytdl-raw-options", "username=user,password=pass")
     end
 end)
 
