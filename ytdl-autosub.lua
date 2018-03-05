68c5f2cc4ce383bd0276a69fd6bac3c26cf8e396
DC-1 (Zaza) <DC-1@tecra.flerp>
2018-03-05 08:39:53 +0100

    use pid

--- ytdl-autosub.lua
+++ ytdl-autosub.lua
@@ -1,23 +1,37 @@
 --
--- download youtue auto-generated english subtitles
+-- download youtube auto-generated english subtitles v0.2
 --   ( copy me to ~/.config/mpv/scripts/ )
 --
 -- required binaries:
 -- youtube-dl   - https://youtube-dl.org/
 --
 local key_binding = "ctrl+v"
+--local autosub = "/dev/shm/mpv-autosub"
+
 local utils = require 'mp.utils'
 local msg = require 'mp.msg'
-local autosub = "/dev/shm/mpv-autosub"
+
+function os.capture(cmd)
+  local f = assert(io.popen(cmd, 'r'))
+  local s = assert(f:read "*a")
+  f:close()
+  return string.sub(s, 0, -2) -- Use string.sub to trim the trailing newline.
+end
+
+function get_pid()
+  return os.capture( 'sh -c "echo $PPID"')
+end
 
 function getautosub()
 	filepath = mp.get_property("path")
 	if (filepath:find("http") ~= 1) then return end
 	msg.warn("download autosub")
+	local autosub = "/dev/shm/" .. get_pid()
         mp.osd_message("downloading auto-generated subtitles", osd_time)
 
         cmd = { args = { 'sh', '-c',
-	    "youtube-dl --skip-download --write-auto-sub -o " .. autosub .. " '" .. filepath .. "'"} }
+	    "youtube-dl --skip-download --write-auto-sub -o " .. autosub ..
+	    " '" .. filepath .. "'"} }
         utils.subprocess(cmd)
 	mp.command("sub_add " .. autosub .. ".en.vtt")
 	os.remove(autosub .. ".en.vtt")
