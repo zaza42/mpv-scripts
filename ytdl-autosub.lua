af89b8a704d74d1ea431aff958bdbba4b90b6228
DC-1 (Zaza) <DC-1@tecra.flerp>
2018-03-20 20:33:29 +0100

    anti overlap subtitle

--- ytdl-autosub.lua
+++ ytdl-autosub.lua
@@ -40,8 +40,13 @@ function getautosub()
 	    return
 	end
 	filepath = mp.get_property("path")
-	if (filepath:find("http") ~= 1) then return end
-	msg.warn("download autosub")
+--	if (filepath:find("http") ~= 1) then return end
+--	sep = "&"
+--	for i in string.gmatch(filepath, "%S+") do
+--	  msg.warn(i)
+--	end
+
+	msg.warn("download autosub " .. filepath)
 	local autosub = "/dev/shm/" .. get_pid()
         mp.osd_message("downloading auto-generated subtitles", osd_time)
 
@@ -49,8 +54,16 @@ function getautosub()
 	    "youtube-dl --skip-download --write-auto-sub -o " .. autosub ..
 	    " '" .. filepath .. "'"} }
         utils.subprocess(cmd)
-	mp.command("sub_add " .. autosub .. ".en.vtt")
+        cmd = { args = { 'sh', '-c',
+	    "ffmpeg -fix_sub_duration -i " .. autosub .. ".en.vtt " .. autosub .. ".srt"} }
+        utils.subprocess(cmd)
+--        cmd = { args = { 'sh', '-c',
+--	    "subfix " .. autosub .. ".srt " .. autosub .. "-fixed.srt"} }
+--        utils.subprocess(cmd)
+	mp.command("sub_add " .. autosub .. ".srt")
 	os.remove(autosub .. ".en.vtt")
+	os.remove(autosub .. ".srt")
+--	os.remove(autosub .. "-fixed.srt")
 	os.remove(autosub .. ".jpg")
 end
 
