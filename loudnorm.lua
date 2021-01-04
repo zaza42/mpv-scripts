02578c541a1171711c3aa15e6e52bfa261612cc5
DC-1 <zaza@sirc.hu>
2021-01-04 10:10:41 +0100

    shortened

--- loudnorm.lua
+++ loudnorm.lua
@@ -1,29 +1,16 @@
 -- settings
 -- key_binding: press the key specified below
 --      to toggle loudnorm filters below
-local key_binding = "ctrl+n"
+local key_binding = "n"
 
 local loudnorm_filter_index = 0
 
 function cycle_loudnorm()
-
-    if loudnorm_filter_index > 0 then
-    -- insert the filter
     ret=mp.command(
         string.format(
-            'af remove lavfi=[loudnorm=I=-16:TP=-3:LRA=4,aresample=48000]'
+            'af toggle lavfi=[loudnorm=I=-16:TP=-3:LRA=4,aresample=48000]'
         )
     )
-    loudnorm_filter_index = 0
-    else
-    -- insert the filter
-    ret=mp.command(
-        string.format(
-            'af add lavfi=[loudnorm=I=-16:TP=-3:LRA=4,aresample=48000]'
-        )
-    )
-    loudnorm_filter_index = 1
-    end
 end
 
 mp.add_key_binding(key_binding, "loudnorm", cycle_loudnorm)
